5fe8cd389d67851e1d36f298bf9b24e46203bf98
ClarenzeJames <clarenzejames@gmail.com>
2025-05-17 01:24:56 +0800

    complete 3055

--- genome.py
+++ genome.py
@@ -13,3 +13,30 @@ class Genome():
     def get_random_genome(gene_length, gene_count):
         genome = [Genome.get_random_gene(gene_length) for i in range(gene_count)]
         return genome
+    
+    @staticmethod
+    def get_gene_spec():
+        gene_spec ={
+            "link-length":{"scale":1},
+            "link-shape":{"scale":1},
+            "link-radius":{"scale":1},
+            "link-recurrence":{"scale":1},
+            "link-mass":{"scale":4},
+            "joint-type":{"scale":1},
+            "joint-parent":{"scale":1},
+            "joint-axis-xyz":{"scale":1},
+            "joint-origin-rpy-1":{"scale":np.pi * 2},
+            "joint-origin-rpy-2":{"scale":np.pi * 2},
+            "joint-origin-rpy-3":{"scale":np.pi * 2},
+            "joint-origin-xyz-1":{"scale":1},
+            "joint-origin-xyz-2":{"scale":1},
+            "joint-origin-xyz-3":{"scale":1},
+            "control-waveform":{"scale":1},
+            "control-amp":{"scale":0.25},
+            "control-freq":{"scale":1}
+        }
+        ind = 0
+        for key in gene_spec.keys():
+            gene_spec[key]["ind"] = ind
+            ind = ind + 1
+        return gene_spec
--- test_genome.py
+++ test_genome.py
@@ -29,4 +29,25 @@ class GenomeTest(unittest.TestCase):
         data = genome.Genome.get_random_genome(20,5)
         self.assertIsNotNone(data)
 
+    # Gene spec things
+
+    def testGeneSpecScale(self):
+        spec = genome.Genome.get_gene_spec()
+        gene = genome.Genome.get_random_gene(20)
+        self.assertGreater(gene[spec["link-length"]["ind"]],0)
+
+    # test for gene and genome to dict
+
+    def testGeneToGeneDict(self):
+        spec = genome.Genome.get_gene_spec()
+        gene  = genome.Genome.get_random_gene(len(spec))
+        gene_dict = genome.Genome.get_gene_dict(gene,spec)
+        self.assertIn("link-reccurence",gene_dict)
+
+    def testGenomeToDict(self):
+        spec = genome.Genome.get_gene_spec()
+        dna  = genome.Genome.get_random_genome(len(spec),3)
+        genome_dicts = genome.Genome.get_genome_dicts(dna,spec)
+        self.assertEqual(len(genome_dicts),3)
+
 unittest.main()
