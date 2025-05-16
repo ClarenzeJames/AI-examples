545ae2aa11344bc876514b8dc9ce43c633110667
ClarenzeJames <clarenzejames@gmail.com>
2025-05-17 01:04:14 +0800

    working initial tests

---
+++ genome.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Genome():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_random_gene(length):
+        gene = np.array([np.random.random() for i in range(length)])
+        return gene
+    
+    @staticmethod
+    def get_random_genome(gene_length, gene_count):
+        genome = [Genome.get_random_gene(gene_length) for i in range(gene_count)]
+        return genome
--- test_genome.py
+++ test_genome.py
@@ -6,4 +6,27 @@ class GenomeTest(unittest.TestCase):
     def testClassExists(self):
         self.assertIsNotNone(genome.Genome)
 
+    ## Code to test the random genome
+    def testRandomGene(self):
+        self.assertIsNotNone(genome.Genome.get_random_gene)
+
+    def testGeneNotNone(self):
+        self.assertIsNotNone(genome.Genome.get_random_gene(5))
+
+    def testRandomGeneHasValue(self):
+        gene = genome.Genome.get_random_gene(5)
+        self.assertIsNotNone(gene[0])
+    
+    def testRandomGeneLength(self):
+        gene = genome.Genome.get_random_gene(20)
+        self.assertEqual(len(gene), 20)
+    
+    def testRandomGeneIsNumpyArray(self):
+        gene = genome.Genome.get_random_gene(20)
+        self.assertEqual(type(gene),np.ndarray)
+
+    def testRandomGenomeExists(self):
+        data = genome.Genome.get_random_genome(20,5)
+        self.assertIsNotNone(data)
+
 unittest.main()
